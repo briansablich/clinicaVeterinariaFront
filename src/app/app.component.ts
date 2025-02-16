d173e68bddbd1a0f31b6f8df4cf12814b5c032eb
Brian_Sablich <sablichbrian@yahoo.com.ar>
2025-02-16 13:32:50 -0300

    RouterLink and homepage created and configurated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 /* 
+-Home
 -Lista de dueños
 -Lista de mascotas (con filtro por raza)
 -Mascota detalle (MascoDuenioDTO con idmascota)
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { MascotasComponent } from './pages/mascotas/mascotas.component';
 import { MascotadetalleComponent } from './pages/mascotadetalle/mascotadetalle.component';
 import { CrearmascotaComponent } from './pages/crearmascota/crearmascota.component';
 import { CrearduenioComponent } from './pages/crearduenio/crearduenio.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    {path: '', component: DueniosComponent},
+    {path: 'duenios', component: DueniosComponent},
     {path: 'mascotas', component: MascotasComponent},
     {path: 'mascotas/detalle', component: MascotadetalleComponent},
     {path: 'crearmascota', component: CrearmascotaComponent},
-    {path: 'crearduenio', component: CrearduenioComponent}
+    {path: 'crearduenio', component: CrearduenioComponent},
+    {path: '', component: HomeComponent}
 ];
