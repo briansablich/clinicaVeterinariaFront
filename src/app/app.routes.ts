6105b3a147d5fce82ad34ef0a58d115e07231d39
Brian_Sablich <sablichbrian@yahoo.com.ar>
2025-02-16 13:09:20 -0300

    Boostrap installed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DueniosComponent } from './pages/duenios/duenios.component';
+import { MascotasComponent } from './pages/mascotas/mascotas.component';
+import { MascotadetalleComponent } from './pages/mascotadetalle/mascotadetalle.component';
+import { CrearmascotaComponent } from './pages/crearmascota/crearmascota.component';
+import { CrearduenioComponent } from './pages/crearduenio/crearduenio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: DueniosComponent},
+    {path: 'mascotas', component: MascotasComponent},
+    {path: 'mascotas/detalle', component: MascotadetalleComponent},
+    {path: 'crearmascota', component: CrearmascotaComponent},
+    {path: 'crearduenio', component: CrearduenioComponent}
+];
