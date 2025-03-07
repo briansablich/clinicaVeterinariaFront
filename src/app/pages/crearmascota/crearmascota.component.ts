875471f06f48a7d8c604d17c268680ee6beb7cbc
Brian_Sablich <sablichbrian@yahoo.com.ar>
2025-03-07 19:12:24 -0300

    Fixed crearmascota

--- src/app/pages/crearmascota/crearmascota.component.ts
+++ src/app/pages/crearmascota/crearmascota.component.ts
@@ -18,29 +18,29 @@ export class CrearmascotaComponent implements OnInit {
   especie: FormControl;
   raza: FormControl;
   color: FormControl;
+  //idDuenio: FormControl;
   idMascotaTraer: number | null = null;
   duenios: Duenio[] = [];
 
 
   constructor(private mascotaService: MascotaService,
     private route: ActivatedRoute,
-    private duenioService: DuenioService,) {
+    private duenioService: DuenioService) {
 
     this.idMascota = new FormControl();
     this.nombre = new FormControl('', Validators.required);
     this.especie = new FormControl('', Validators.required);
     this.raza = new FormControl('', Validators.required);
     this.color = new FormControl('', Validators.required);
+    //this.idDuenio = new FormControl('');
 
     this.mascotaForm = new FormGroup({
       idMascota: this.idMascota,
       nombre: this.nombre,
       especie: this.especie,
       raza: this.raza,
-      color: this.color,
-      duenio: new FormControl({
-        idDuenio: new FormControl()
-      })
+      color: this.color
+      //idDuenio: new FormControl()
     })
   }
 
